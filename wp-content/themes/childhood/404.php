2c64bdf5ff2fc822d23e41dfa36059d26cbf525d
danila <zaharchuk.danila18@gmail.com>
2024-01-04 13:55:24 +0300

    projects v1

--- wp-content/themes/childhood/404.php
+++
@@ -1,9 +0,0 @@
-<?php
-get_header();
-?>
-<div class="container toys">
-    <h2 class="subtitle">
-        <a href="<?php echo home_url(); ?>">Такой страницы не существует. Ввернуться на главную?</a>
-    </h2>
-</div>
-<?php get_footer(); ?>
